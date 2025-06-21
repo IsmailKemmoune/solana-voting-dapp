2cbffbae88424e6a3db7840dc175cbbdd7299df9
Ismail Kemmoune <i.kemmoune@gmail.com>
2025-06-21 23:29:57 +0100

    test: add test for the initialize poll transaction handler

---
+++ anchor/tests/voting.test.ts
@@ -0,0 +1,27 @@
+import { BankrunProvider, startAnchor } from 'anchor-bankrun'
+import { PublicKey } from '@solana/web3.js'
+import { BN, Program } from '@coral-xyz/anchor'
+import { Voting } from '../target/types/voting'
+import IDL from '../target/idl/voting.json'
+import { expect } from '@jest/globals'
+
+const votingAddress = new PublicKey('FqzkXZdwYjurnUKetJCAvaUw5WAqbwzU6gZEwydeEfqS')
+
+describe('Voting', () => {
+  it('Initialize Poll', async () => {
+    const context = await startAnchor('', [{ name: 'voting', programId: votingAddress }], [])
+
+    const provider = new BankrunProvider(context)
+
+    const votingProgram = new Program<Voting>(IDL, provider)
+
+    await votingProgram.methods.initializePoll(new BN(1), 'this is a description', new BN(0), new BN(1850536880)).rpc()
+
+    const [pollAddress] = PublicKey.findProgramAddressSync([new BN(1).toArrayLike(Buffer, 'le', 8)], votingAddress)
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress)
+    console.log(poll, 'poll')
+
+    expect(poll.pollId.toNumber()).toEqual(1)
+  })
+})
