a2a10b34cbaac784d7b5515aa9d08685ca8186ba
Ismail Kemmoune <i.kemmoune@gmail.com>
2025-06-29 22:28:27 +0100

    feat: add get request that returns oslana action metadata

---
+++ src/app/api/vote/route.ts
@@ -0,0 +1,19 @@
+export async function GET(request: Request) {
+  const actionMetadata = {
+    label: 'Vote',
+    icon: 'https://www.longwarjournal.org/wp-content/uploads/2025/06/israel-vs-iran-800.jpg',
+    title: "Who's going to win the war?",
+    description: 'Vote between Iran and Israel',
+  }
+
+  return Response.json(actionMetadata, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,OPTIONS',
+      'Access-Control-Allow-Headers':
+        'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids',
+      'Access-Control-Expose-Headers': 'X-Action-Version, X-Blockchain-Ids',
+      'Content-Type': 'application/json',
+    },
+  })
+}
