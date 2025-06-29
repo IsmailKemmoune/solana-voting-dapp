5cfee0684eb284e07cf1f31aaf3713848596b086
Ismail Kemmoune <i.kemmoune@gmail.com>
2025-06-29 22:45:06 +0100

    feat: add the vote actions to the links property

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,9 +1,23 @@
+export const OPTIONS = GET
+
 export async function GET(request: Request) {
   const actionMetadata = {
     label: 'Vote',
     icon: 'https://www.longwarjournal.org/wp-content/uploads/2025/06/israel-vs-iran-800.jpg',
     title: "Who's going to win the war?",
     description: 'Vote between Iran and Israel',
+    links: {
+      actions: [
+        {
+          label: 'Vote for Iran',
+          href: '/api/vote?candidate=iran',
+        },
+        {
+          label: 'Vote for Israel',
+          href: '/api/vote?candidate=israel',
+        },
+      ],
+    },
   }
 
   return Response.json(actionMetadata, {
